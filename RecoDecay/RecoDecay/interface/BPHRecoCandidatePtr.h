7d6b142cb13a975622eadb28e53a35bc2bf2c6ca
Lian-Sheng <johnson20050@gmail.com>
2017-07-30 06:23:32 +0800

    delete useless folder

--- RecoDecay/RecoDecay/interface/BPHRecoCandidatePtr.h
+++
@@ -1,9 +0,0 @@
-#ifndef BPHAnalysis_RecoDecay_BPHRecoCandidatePtr_h
-#define BPHAnalysis_RecoDecay_BPHRecoCandidatePtr_h
-
-#include "BPHAnalysis/RecoDecay/interface/BPHGenericPtr.h"
-class BPHRecoCandidate;
-typedef BPHGenericPtr<      BPHRecoCandidate>::type BPHRecoCandidatePtr;
-typedef BPHGenericPtr<const BPHRecoCandidate>::type BPHRecoConstCandPtr;
-
-#endif
